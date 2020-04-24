541607a2e1ae52d15d17b6ac61b2f4be224ec611
dwang78 <dwang78@gmail.com>
2020-04-23 20:32:59 -0700

    2.3 updated

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+console.log("Hi Asshole");
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
